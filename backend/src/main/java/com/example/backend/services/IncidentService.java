7225f6d20578fd1f53debb175bd68040c50decb7
janamohamed2304 <janamohamed2304@gmail.com>
2025-11-30 21:36:13 +0200

    EM-7-Added Assignment Service and HaversineFormula shared class

---
+++ backend/src/main/java/com/example/backend/services/IncidentService.java
@@ -0,0 +1,34 @@
+package com.example.backend.services;
+
+
+import com.example.backend.entities.Incident;
+import com.example.backend.repositories.IncidentRepository;
+import static com.example.backend.enums.IncidentStatus.REPORTED;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+
+@Service
+public class IncidentService {
+
+    @Autowired
+    private IncidentRepository incidentRepository;
+
+//    @Autowired
+//    private DispatchService dispatchService;
+//
+//    public Incident createIncident(Incident incident){
+//
+//        incident.setStatus(REPORTED);
+//        incident.setReportedAt(LocalDateTime.now());
+//        Incident savedIncident = incidentRepository.save(incident);
+//
+//        dispatchService.tryAutoAssign(savedIncident);
+//
+//        return savedIncident;
+//    }
+
+    // add other methods on contract
+}
