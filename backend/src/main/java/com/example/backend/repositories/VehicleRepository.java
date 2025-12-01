b66376b66510715f087239cf432b68be935e6316
NourrKhalled <nourrkhalled123@gmail.com>
2025-12-01 02:28:26 +0200

    EM-7-Added Vehicle Controller

--- backend/src/main/java/com/example/backend/repositories/VehicleRepository.java
+++ backend/src/main/java/com/example/backend/repositories/VehicleRepository.java
@@ -3,7 +3,6 @@ package com.example.backend.repositories;
 import com.example.backend.entities.Vehicle;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -12,17 +11,20 @@ import org.springframework.stereotype.Repository;
 
 import com.example.backend.enums.IncidentType;
 import com.example.backend.enums.VehicleStatus;
+import com.example.backend.enums.VehicleType;
 
 
 
 @Repository
 public interface VehicleRepository extends JpaRepository<Vehicle, Integer> {
     
-    @Query("SELECT vehicle FROM Vehicle vehicle WHERE vehicle.type = :type AND vehicle.status = 'available'")
+    @Query("SELECT v FROM Vehicle v WHERE v.type = :type AND v.status = VehicleStatus.AVAILABLE")
     List<Vehicle> findAvailableByType(@Param("type") IncidentType type);
 
     List<Vehicle> findByStatus(VehicleStatus status);
 
-    Optional<Vehicle> findById(Integer id);
+    List<Vehicle> findByType(VehicleType type);
+
+    List<Vehicle> findByStatusAndType(VehicleStatus status, VehicleType type);
 
 }
