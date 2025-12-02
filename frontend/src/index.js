db8b93e02ced542eea7cca81797e3b4c6ea2fe06
Menan <aboeldahabmenan@gmail.com>
2025-12-02 22:13:17 +0200

    feat: add report incident form

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import IncidentForm from './IncedintForm';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <IncidentForm />
   </React.StrictMode>
 );
 
