c133e191e3236c5f2bde9e36bcbd14d4d9652af8
ShahdMohamed-11 <shahdmohamed7377@gmail.com>
2025-12-03 13:21:49 +0200

    Add untracked file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,16 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import VehicleManagement from './VehicleManagement';
+import IncedintForm from './IncedintForm';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<VehicleManagement />} />
+        <Route path="/admin" element={<IncedintForm />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import IncidentForm from './IncedintForm';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <IncidentForm />
+    <App />
   </React.StrictMode>
 );
 
