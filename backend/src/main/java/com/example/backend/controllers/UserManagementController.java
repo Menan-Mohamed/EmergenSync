e2805c72e0c0df9506a9d7cc02a5c63a7b26badd
Menan <aboeldahabmenan@gmail.com>
2025-12-04 01:35:26 +0200

    filter-api-byRole

--- backend/src/main/java/com/example/backend/controllers/UserManagementController.java
+++ backend/src/main/java/com/example/backend/controllers/UserManagementController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/users")
-@CrossOrigin(origins = "http://localhost:3000")
+//@CrossOrigin(origins = "http://localhost:3000")
 public class UserManagementController {
 
     @Autowired
